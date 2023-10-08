1e663e89276f3dc3ad0dd14c95a338e04a9e2d1f
Ze Hao Ah <zah@u91z06s01.42kl.edu.my>
2023-10-08 12:05:14 +0800

    Left reminder:

--- backend/src/events/events.controller.ts
+++ backend/src/events/events.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Body, Headers, Post } from "@nestjs/common";
+import { Controller, Body, Get, Headers, Post, Param } from "@nestjs/common";
 import { EventsService } from "./events.service";
 import { eventDataDTO, eventHoverDTO, eventCreationDTO } from "src/dto/event.dto";
 
@@ -6,21 +6,53 @@ import { eventDataDTO, eventHoverDTO, eventCreationDTO } from "src/dto/event.dto
 export class EventsController{
   constructor(private readonly EventsService: EventsService){}
 
-  // @Get()
-  // async getAllEvents(@Headers("Authorization") ): Promise<eventDataDTO[]>{
+  @Get("my")
+  async getMyEvents(@Headers("Authorization") token: string): Promise<any>{
+    return (await this.EventsService.getMyEvents(token));
+  }
 
-  // }
+  @Get("suggested")
+  async getSuggestedEvents(@Headers("Authorization") token: string): Promise<any>{
+    return (await this.EventsService.getSuggestedEvents(token));
+  }
 
-  @Post()
-  async createEvent(@Body() eventCreationDTO: eventCreationDTO, @Headers("Authorization") token: string): Promise<any>{
+  @Get("today")
+  async getTodayEvents(@Headers("Authorization") token: string): Promise<any>{
+    console.log("today event")
+    return (await this.EventsService.getTodayEvents(token));
+  }
+
+  @Get("week")
+  async getWeekEvents(@Headers("Authorization") token: string): Promise<any>{
+    console.log("week event")
+    return (await this.EventsService.getWeekEvents(token));
+  }
+
+  @Get("month")
+  async getMonthEvents(@Headers("Authorization") token: string): Promise<any>{
+    console.log("month event")
+    return (await this.EventsService.getMonthEvents(token));
+  }
+
+  @Get("id/:id")
+  async getEventByID(@Headers("Authorization") token: string, @Param("id") id: string): Promise<any>{
+    return (await this.EventsService.getEventByID(token, id));
+  }
+
+  @Post("create")
+  async createEvent(@Body() eventCreationDTO: eventCreationDTO, @Headers("Authorization") token: string): Promise<number>{
+    console.log("Create event");
+    console.log(eventCreationDTO);
     return (await this.EventsService.createEvent(eventCreationDTO, token));
   }
 
+  @Post("join")
+  async joinEvent(@Body() body: any, @Headers("Authorization") token: string): Promise<any>{
+    return (await this.EventsService.joinEvent(body.eventID, token));
+  }
 
-  //GetMy
-  //GetSuggested
-  //GetToday
-  //GetThisWeek
-  //GetThisMonth
-  //GetById
+  @Get("reset")
+  async resetEvents(): Promise<void>{
+    return (await this.EventsService.resetEvents());
+  }
 }
