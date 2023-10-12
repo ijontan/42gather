d30a7e19789de72987ff9e201ba1d77132bc0a4d
Ze Hao Ah <zah@u91z06s01.42kl.edu.my>
2023-10-12 13:45:24 +0800

    Change cors origin

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule, {
       cors:{
-        origin: "*",
+        origin: "https://42gather.vercel.app/",
         methods: ["GET","POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
       }
