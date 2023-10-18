c8a78155d89115a15562957b67640912c1ccb515
Raspberrynani <daryl11378@gmail.com>
2023-10-19 06:18:24 +0800

    Please help

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,10 +9,15 @@ async function bootstrap() {
         origin: "*",
         methods: ["GET","POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
-        allowedHeaders: "*",
+        allowedHeaders:'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
       }
+
+  
   });
   app.enableCors();
   await app.listen(3000);
+  if (request.method === 'OPTIONS') {
+    return response.status(200).send('ok');
+  }
 }
 bootstrap();
