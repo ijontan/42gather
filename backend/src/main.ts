93b4a342e52017e41053515c3a87e1f423f87cfa
Raspberrynani <daryl11378@gmail.com>
2023-10-19 06:28:51 +0800

    PLS HELP ME FOR THE LOVE OF GOD

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,15 +9,12 @@ async function bootstrap() {
         origin: "*",
         methods: ["GET","POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         credentials: true,
-        allowedHeaders:'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
+        allowedHeaders:'*',
       }
 
   
   });
   app.enableCors();
   await app.listen(3000);
-  if (request.method === 'OPTIONS') {
-    return response.status(200).send('ok');
-  }
 }
 bootstrap();
